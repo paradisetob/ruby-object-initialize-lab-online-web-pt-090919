67d0a8636a0122928c5fdccfbf05fa96b5117405
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 09:42:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name,breed)
+    @name=name
+    @breed=breed
+  end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+
+  fido= name.new("fido")
+  fido=breed.new("Pug")
+  
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class person
+  def initialize(name)
+    @name="beyonce"
+  end
+  def person=(name)
+    @name=person
+end
+def person
+  @person
+  end
+  beyone=person.new
